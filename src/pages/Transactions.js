b90a06877dfa0f611fe24254e7ffa2ab5fe55ec8
Shedrack Okunda <okundashedrack@gmail.com>
2024-10-14 16:01:20 +0300

    i changed the table from table to datagrid

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,24 +1,11 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  Chip,
-  IconButton,
-  Pagination,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, IconButton, TextField, Typography } from "@mui/material";
 import { FilterList, Search } from "@mui/icons-material";
+import { DataGrid } from "@mui/x-data-grid";
 
-const transactions = [
+const rows = [
   {
+    id: 1,
     date: "Aug 03",
     sender: "Nnabuife Elijah",
     recipient: "Elijah",
@@ -27,6 +14,7 @@ const transactions = [
     type: "Transfer",
   },
   {
+    id: 2,
     date: "Aug 03",
     sender: "Nnabuife Elijah",
     recipient: "Elijah",
@@ -35,6 +23,7 @@ const transactions = [
     type: "Transfer",
   },
   {
+    id: 3,
     date: "Aug 03",
     sender: "Nnabuife Elijah",
     recipient: "Elijah",
@@ -43,6 +32,7 @@ const transactions = [
     type: "Fund Wallet",
   },
   {
+    id: 4,
     date: "Aug 03",
     sender: "Nnabuife Elijah",
     recipient: "Elijah",
@@ -51,6 +41,7 @@ const transactions = [
     type: "Transfer",
   },
   {
+    id: 5,
     date: "Aug 03",
     sender: "Nnabuife Elijah",
     recipient: "Elijah",
@@ -60,81 +51,99 @@ const transactions = [
   },
 ];
 
+const columns = [
+  { field: "id", headerName: "ID", width: 50 },
+  { field: "date", headerName: "Date", editable: true },
+  {
+    field: "sender",
+    headerName: "Sender",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "amount",
+    headerName: "Amount",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "recipient",
+    headerName: "Recipient",
+    width: 150,
+    editable: true,
+  },
+  { field: "status", headerName: "Status", width: 110, editable: true },
+
+  {
+    field: "type",
+    headerName: "Transaction Type",
+    width: 160,
+  },
+];
+
 function Transaction() {
   return (
-    <Box sx={{ padding: "20px" }}>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        mb={3}
-        gap={2}
-      >
-        <TextField
-          variant="outlined"
-          size="small"
-          placeholder="Search for a transaction"
-          InputProps={{
-            startAdornment: (
-              <IconButton>
-                <Search />
-              </IconButton>
-            ),
+    <Box component="div" sx={{ p: 3 }}>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Box
+          sx={{
+            margin: "15px",
+            display: "flex",
+            alignItems: "center",
+            gap: 2,
+            width: "100%",
           }}
-          sx={{ width: "100%" }}
-        />
-        <Button variant="outlined" endIcon={<FilterList />}>
-          Filter
-        </Button>
+        >
+          <TextField
+            placeholder="Search for a transaction"
+            InputProps={{
+              startAdornment: (
+                <IconButton>
+                  <Search />
+                </IconButton>
+              ),
+            }}
+            sx={{
+              backgroundColor: "#f9f9f9",
+              borderRadius: "8px",
+              width: { xs: "30%", md: "100%" },
+            }}
+          />
+          <IconButton
+            sx={{
+              borderRadius: "10px",
+              height: "55px",
+              padding: "10px 30px",
+              border: "1px solid #ddd",
+              width: { xs: "auto", md: "auto" },
+            }}
+          >
+            <Typography sx={{ mr: "2px" }}>Filter</Typography>
+            <FilterList />
+          </IconButton>
+        </Box>
       </Box>
 
-      <Typography variant="h6" mb={2}>
+      <Box sx={{ height: 400, width: { xs: "100%", md: "900px" } }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+        ></DataGrid>
+      </Box>
+
+      {/* <Typography variant="h6" mb={2}>
         Recent Transactions
-      </Typography>
-
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Date</TableCell>
-              <TableCell>Sender</TableCell>
-              <TableCell>Amount</TableCell>
-              <TableCell>Recipient</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Type</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {transactions.map((tx, index) => (
-              <TableRow key={index}>
-                <TableCell>{tx.date}</TableCell>
-                <TableCell>{tx.sender}</TableCell>
-                <TableCell>{tx.amount}</TableCell>
-                <TableCell>{tx.recipient}</TableCell>
-                <TableCell>
-                  <Chip
-                    label={tx.status}
-                    color={tx.status === "Successful" ? "success" : "default"}
-                    variant="outlined"
-                  />
-                </TableCell>
-                <TableCell>{tx.type}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        mt={3}
-      >
-        <Typography variant="body2">1-5 of 5</Typography>
-        <Pagination count={1} page={1} />
-      </Box>
+      </Typography> */}
     </Box>
   );
 }
