4e801afd7d99fe96ec2e6c1ff242b24e71b85da2
Shedrack Okunda <okundashedrack@gmail.com>
2024-10-15 16:57:06 +0300

    created the card component and styled it

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,10 +1,50 @@
+import { CreditCard } from "@mui/icons-material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import React from "react";
 
 function Card() {
   return (
-    <>
-      <h1>Hello Country</h1>
-    </>
+    <Box sx={{ p: 4 }}>
+      <Paper>
+        <Box>
+          <Box
+            sx={{
+              p: 2,
+              borderBottom: "2px solid #ccc",
+              backgroundColor: "#f0e6ff",
+            }}
+          >
+            <Typography>Cards</Typography>
+          </Box>
+
+          <img src={require("./card.png")} alt="card" height="310px" />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{
+                padding: "10px 50px",
+                marginBottom: "20px",
+                backgroundColor: "#7105E9",
+                margin: "20px",
+                width: "100%",
+                textTransform: "none",
+                fontWeight: 600,
+                fontSize: "18px",
+              }}
+              startIcon={<CreditCard />}
+            >
+              Create new card
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
   );
 }
 
